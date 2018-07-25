c8afc77c6784b0b0c8403d48947de8e7d9f12c0b
Ben Cronin <benc@bens-mbp.lan>
2018-07-25 09:15:46 -0700

    Configure server-side static resource routing

--- proxy/server.js
+++ proxy/server.js
@@ -1,11 +1,14 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/search", express.static(path.join(__dirname, "public")));
+app.use("/search/:searchId", express.static(path.join(__dirname, "public")));
+app.use("/listing/:listingId", express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
