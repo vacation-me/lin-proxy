f991d803e34b8351d73ca06dcdde76fc8068b0df
Ben Cronin <benc@bens-mbp.lan>
2018-07-27 10:16:46 -0700

    Refactor proxy to render components in index.html

--- proxy/server.js
+++ proxy/server.js
@@ -6,9 +6,8 @@ const port = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 
-app.get("*", function(req, res) {
-  res.sendFile(path.resolve(`${__dirname}/public`, "index.html"));
-});
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/listing/:id", express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
