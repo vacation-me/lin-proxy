51732f16086d564312019eae87acd74614f32b70
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-07-31 16:55:50 -0700

    Simplify static file service

--- proxy/server.js
+++ proxy/server.js
@@ -5,9 +5,7 @@ const port = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 
-app.use("/", express.static(`${__dirname}/public`));
-app.use("/search/:searchQuery", express.static(`${__dirname}/public`));
-app.use("/listing/:listingId", express.static(`${__dirname}/public`));
+app.use("/*", express.static(`${__dirname}/public`));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
