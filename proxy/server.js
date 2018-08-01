e306e02624656551e26037be8e772055d3b7e343
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-01 14:52:29 -0700

    Fine-tune docker configurations

--- proxy/server.js
+++ proxy/server.js
@@ -5,7 +5,9 @@ const port = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 
-app.use(express.static(`${__dirname}/public`));
+app.use("/", express.static("public"));
+app.use("/listing/:listingId", express.static("public"));
+app.use("/search/:searchQuery", express.static("public"));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
