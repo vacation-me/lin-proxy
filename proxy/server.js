c32135a679405c6bac8f88169984cec078d1a275
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-01 12:18:14 -0700

    Set up docker configuration

--- proxy/server.js
+++ proxy/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 
-app.use("/*", express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
