58f46a10697dc9bb34994f932e811849f9bab355
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-02 10:24:24 -0700

    Clean up public/ files

---
+++ proxy/public/utils.js
@@ -0,0 +1,26 @@
+var debounce = function(func, timeout) {
+  var callable = true;
+  return function() {
+    if (callable) {
+      func();
+      callable = false;
+      setTimeout(function() {
+        callable = true;
+      }, timeout);
+    }
+  };
+};
+
+var scrollSticky = function() {
+  var booking = document.getElementById("book");
+  if (booking) {
+    var sticky = booking.offsetTop;
+    if (window.pageYOffset > sticky - 12) {
+      booking.children[0].classList.add("sticky");
+    } else {
+      booking.children[0].classList.remove("sticky");
+    }
+  }
+};
+
+window.onscroll = debounce(scrollSticky, 2);
